080b4db120b19fbcf88546a73a5611e4cfc1fdab
jujiva <martincampagner@hotmail.com>
2019-05-17 15:37:49 -0300

    en espaniol x fabor

--- src/database.js
+++ src/database.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose')
 const { database } = require('./keys')
 
 mongoose.connect(database.URI, { useNewUrlParser: true })
-    .then(db => console.log('DB is connected'))
+    .then(db => console.log('Conectado a la base de datos'))
     .catch(err => console.error(err))
