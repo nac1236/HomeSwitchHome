5c661cfa5e2756d6882b762dc7c6fc25e9f65633
Nacho <nacla12@hotmail.com>
2019-05-18 22:43:04 -0300

    Agrega metodo put para modificacion

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,17 +10,20 @@ module.exports = app => {
     /* PROPIEDADES */
     router.get('/propiedad/:propiedad_id')
     router.post('/propiedad/')
+    router.put('/propiedad/')
     router.delete('/propiedad/:propiedad_id')
 
     /* SUBASTAS */
     router.get('/propiedad/:propiedad_id/subasta/:subasta_id')
     router.post('/propiedad/:propiedad_id/subasta/')
+    router.put('/propiedad/')
     router.delete('/propiedad/:propiedad_id/subasta/:subasta_id')
 
     /* HOTSALES */
 
     router.get('/propiedad/:propiedad_id/hotsale/:hotsale_id')
     router.post('/propiedad/:propiedad_id/hotsale/')
+    router.put('/propiedad/')
     router.delete('/propiedad/:propiedad_id/hotsale/:hotsale_id')
 
 
