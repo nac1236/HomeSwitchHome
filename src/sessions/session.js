4a7feb1dea0689abba30e4d5d68cd0eae21c151d
Nacho <nacla12@hotmail.com>
2019-05-20 18:43:47 -0300

    Se modifico el manejo de sesion. Hay que agregar un login

--- src/sessions/session.js
+++ src/sessions/session.js
@@ -6,37 +6,6 @@ const router = express.Router()
 
 module.exports = app => {
 
-    const usuarios = [  
-        {id : 1345, email:'admin@gmail.com', password : '123456'  }
-    ]
-
-    const redirectLogin = (req,res,next) => {
-        if(!req.session.user_ID){
-            res.json('No esta logueado, no puede acceder aqui!')
-            //deberia hacer un res.redirect a /login
-        }else{
-            next()
-        }
-    } //esta funcion es por ejemplo para que no entre a la pagina si no esta logueado
-
-    router.get('/login', (req,res) => {
-        res.json('recibido')
-    })
-
-    router.post('/login/:user_ID',(req,res) =>{
-        const user_ID = req.params.user_ID
-        if (user_ID){ //si se paso un user id como parametro
-            const usuario = usuarios.find(
-                usuario => usuario.id = user_ID
-            )
-        if(usuario){
-            req.session.user_ID = usuario.id
-            res.json('Usuario Admin logueado.')
-        }
-        } 
-        res.redirect('/login')
-    } )
-
     app.use(session({
         name: 'sid',
         resave: false, //por defecto viene en true
@@ -47,6 +16,61 @@ module.exports = app => {
             //secure: IN_PROD
         }
     }))
+
+    const usuarios = [  
+        {id : 1345, email:'admin@gmail.com', password : '123456' }
+    ]
+
+    /*const redirectLogin = (req,res,next) => {
+        if(!req.session.user_ID){
+            res.json('No esta logueado, no puede acceder aqui!')
+            //deberia hacer un res.redirect a /login
+        }else{
+            next()
+        }
+    } //esta funcion es por ejemplo para que no entre a la pagina si no esta logueado
+
+    router.get('/login/session', (req,res) => {
+        res.json(req.session.userId)
+    })*/
+
+    router.post('/login/',(req,res) =>{ 
+        const user_ID = req.body.user_ID
+        if (user_ID){ //si se paso un user id como parametro
+            const usuario = usuarios.find(
+                usuario => usuario.id = user_ID
+            )
+        if(usuario){
+            req.session.userId = usuario.id; 
+            res.json(req.session.userId); 
+        }
+        } 
+        //res.redirect('/login')
+    } )
+
+   app.post('/logout', (req, res) => {
+        // Assuming the request was authenticated in /login above,
+         console.log(session.user);
+          session ({
+            cookie: {
+              path: '/',
+              originalMaxAge: 7200000,
+              httpOnly: true,
+              secure: false,
+              sameSite: true
+            }
+          })
+          req.session.destroy(err => {
+            // We can also clear out the cookie here. But even if we don't, the
+            // session is already destroyed at this point, so either way, they
+            // won't be able to authenticate with that same cookie again.
+            res.clearCookie('sid')
+            res.redirect('/')
+          })
+        })
+
+   
+
 
     app.use(router)
 }
