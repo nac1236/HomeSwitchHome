66e18edefbe229216069fa77e90973428c1511d7
Ezequiel D <ezequielensenada@yahoo.com.ar>
2019-07-01 17:33:32 -0300

    Interfaz

--- src/app/usuarioapp.js
+++ src/app/usuarioapp.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { Link } from 'react-router-dom'
-import ListaDeUsuarios from './components/listaDeUsuarios';
-import Navbar from './components/Navbar';
+import ListaDeUsuarios from './components/listaDeUsuarios'
+import Navbar from './components/Navbar'
+
 
 class ActionUsuarios extends Component{
   constructor() {
@@ -18,17 +19,35 @@ class ActionUsuarios extends Component{
                   <div>
                      <h5 align="center">Usuarios</h5>
                   </div>
-                 </header>
-               </div>
+                </header>
+              </div>
+              <div className="row">
+                 <div className="col s4 ">
+                     <label>Filtrar por nombre:</label>
+                     <input type="text" size="15"></input>
+                 </div>
+                 <div className="col s4">
+                     <label>Filtrar por fecha de registro:</label>
+                     <input type="date" size="15"></input>
+                 </div>
+                 <div className="col s2 ">
+                     <label>Estandar</label>
+                     <input type="checkbox"></input>
+                 </div>
+                 <div className="col s2 ">
+                     <label>Premium</label>
+                     <input type="checkbox"></input>  
+                 </div>
+              </div>
+              <div>
+                   <button className=" indigo accent-1">
+                      Cambiar Precio
+                    </button>
+              </div>
               <div>
                 <ListaDeUsuarios>
                  </ListaDeUsuarios>
                </div>    
-                <div>
-                   <button className=" indigo accent-1">
-                      Cambiar Precio
-                    </button>
-                </div>
             </div>
         )
     }
