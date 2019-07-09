95e13f9de17e4205c780f5423317db52ef8d1377
jujiva <martincampagner@hotmail.com>
2019-07-09 15:00:25 -0300

    ID de propiedad en detalle de la misma

--- src/app/components/DetallePropiedad.js
+++ src/app/components/DetallePropiedad.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
 
 export default class DetallePropiedad extends Component {
-    constructor(){
-        super()
+    constructor(props){
+        super(props)
+        console.log(props)
+    }
+
+    componentDidMount(){
+        fetch(`/api/propiedad/${this.props.match.params.propId}`)
+            .then(res => res.json())
+            .then(data => console.log(data))
     }
 
     render() {
         return (
             <div>
-                
+        
             </div>
         )
     }
