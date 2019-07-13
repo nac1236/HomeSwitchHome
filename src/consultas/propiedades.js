728c7b4900febbf0829eeaa685a9aa867824b928
Nacho <nacla12@hotmail.com>
2019-07-13 18:27:40 -0300

    Modificar costo de propiedades

--- src/consultas/propiedades.js
+++ src/consultas/propiedades.js
@@ -61,7 +61,7 @@ ctrl.modifyDescripcion =  async (req,res) => {
     res.json('Recibido')
 }
 
-ctrl.modifyDescripcion =  async (req,res) => {
+ctrl.modifyCosto =  async (req,res) => {
     await Propiedad.findOneAndUpdate({_id: req.params.propiedad_id},{costo : req.body.costo})
     res.json('Recibido')
 }
