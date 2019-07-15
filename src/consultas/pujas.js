a8e16eea516bcc521a7347f4fb68f127affad667
Nacho <nacla12@hotmail.com>
2019-07-14 23:59:33 -0300

    Cambios para generar la finalizacion de las subastas

--- src/consultas/pujas.js
+++ src/consultas/pujas.js
@@ -1,5 +1,6 @@
  const Pujas = require ('../models/pujas')
  const Subasta = require('../models/subastas')
+ const Usuario = require ('../models/usuarios')
  
  ctrlPuja = {}
 
@@ -35,6 +36,48 @@
 
  //cuando se termine el tiempo de puja hay que setear la semana como ocupada si alguien la gano o solo dejar invalida la subasta
 //recordar analizar si el usuario tiene fondos y creditos suficientes (para reservas y subastas)
+
+ctrlPuja.finalizar = async (subasta) =>{
+   const pujas = await Pujas.find({subasta_id: subasta._id})
+   pujas.sort(pujas.monto_actual)
+   var ok = false
+   var usuario
+   var indice = 0
+   while(!ok){
+      usuario = await Usuario.findById({_id: pujas[indice].usuario_id})
+      var tarjeta = await Tarjeta.find({usuario_id: usuario._id})
+      if (tarjeta.credito >= pago.monto){
+         if(usuario.creditos > 0){
+            cobrar(usuario,tarjeta,pujas[indice].monto)
+            marcarOcupada(reserva.id)
+            await pago.save()
+            //res.json('Pago realizado.')
+            ok = true 
+         }else{
+            //res.json('No se tienen creditos suficientes.')
+            ok = false
+         }
+      }else{
+         //res.json('La tarjeta no tiene fondos suficientes.')
+         ok = false
+      }
+
+      indice++
+   }
+} 
+
+
+cobrar = async (usuario,tarjeta,monto) => {
+   usuario.creditos = usuario.creditos - 1
+   tarjeta.credito = tarjeta.credito - monto
+   await Tarjeta.findByIdAndUpdate({_id:tarjeta._id}, {credito: tarjeta.credito})
+   await Usuario.findByIdAndUpdate({_id: usuario._id}, {creditos : usuario.creditos})
+}
+
+marcarOcupada = async (subastaId) =>{
+   const subasta = await Subasta.findByIdAndUpdate({_id: subastaId}, { valida: false})
+   await Semana.findByIdAndUpdate({_id: subasta.semana_reserva}, {disponible: false})
+}
 
 ctrlPuja.deleteAll = async (req,res) => {
    await Puja.deleteMany({__v : 0 })
