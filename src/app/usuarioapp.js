faf0659e941d71cd524a4d9f176dfa368a97d581
jujiva <martincampagner@hotmail.com>
2019-07-16 07:58:00 -0300

    agregados checkboxes en usuarioapp

--- src/app/usuarioapp.js
+++ src/app/usuarioapp.js
@@ -36,30 +36,38 @@ class ActionUsuarios extends Component {
               <input type="date"></input>
             </div>
           </div>
-          <div className="input-field col s12">
-            <p>
-              <input id="premium" type="radio" name="suscripcion"
-                value="premium" defaultChecked />
-              <label htmlFor="premium">Male</label>
-            </p>
-
-            <p>
-              <input id="estandar" type="radio" name="suscripcion"
-                value="estandar" defaultChecked />
-              <label htmlFor="estandar">Estándar</label>
-            </p>
-
-            <p>
-              <input id="ambos" type="radio" name="suscripcion"
-                value="ambos" defaultChecked />
-              <label htmlFor="ambos">Ambos</label>
-            </p>
+          <div className="row">
+            <div className="col s3">
+              <div className="input-field col s12">
+                <p>
+                  <label htmlFor="estandar">
+                    <input id="estandar" type="radio" name="suscripcion"
+                      value="estandar" defaultChecked />
+                    <span>Estándar</span>
+                  </label>
+                </p>
+                <p>
+                  <label htmlFor="premium">
+                    <input id="premium" type="radio" name="suscripcion"
+                      value="premium" defaultChecked />
+                    <span>Premium</span>
+                  </label>
+                </p>
+                <p>
+                  <label htmlFor="ambos">
+                    <input id="ambos" type="radio" name="suscripcion"
+                      value="ambos" defaultChecked />
+                    <span>Ambos</span>
+                  </label>
+                </p>
+              </div>
+            </div>
           </div>
         </form>
         <div>
           <button className=" indigo accent-1">
             Cambiar Precio
-                    </button>
+          </button>
         </div>
         <div>
           <ListaDeUsuarios />
